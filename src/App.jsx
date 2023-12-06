666eb42e2f4407b53981f1ef7da6c1fa652646ad
Dustin Hurtt <dustin_hurtt@outlook.com>
2023-12-06 10:29:38 -0500

    lesson done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import ToDoList from './components/ToDoList'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <ToDoList />
+    </div>
+  )
+}
+
+export default App
